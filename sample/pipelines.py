c674f6d9ece38fc45b0911bccc72b2aa32805037
Sudheer Keshav Bhat <sudheer.k.bhat@gmail.com>
2020-11-09 10:34:49 +0000

    saving to sqlite3

--- sample/pipelines.py
+++ sample/pipelines.py
@@ -5,10 +5,27 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
 from itemadapter import ItemAdapter
 
 
 class SamplePipeline:
+	def __init__(self):
+		self.create_connection()
+		self.create_table()
+
+	def create_connection(self):
+		self.conn = sqlite3.connect('myquotes.db')
+		self.curr = self.conn.cursor()
+
+	def create_table(self):
+		self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+		self.curr.execute("""CREATE TABLE quotes_tb(title text, author text, tag text)""")
+
 	def process_item(self, item, spider):
-		print("Pipeline:" + item['title'][0])
+		self.store_db(item);
 		return item
+
+	def store_db(self, item):
+		self.curr.execute("""INSERT INTO quotes_tb VALUES(?, ?, ?)""",
+			(item['title'][0], item['author'][0], item['tag'][0]))
